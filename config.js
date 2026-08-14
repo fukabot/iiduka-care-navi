d51fd07671036a91535ee28d4ac67b58fd869342
fukabot <163494918+fukabot@users.noreply.github.com>
2026-08-14 16:41:33 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+// ここに自分のGoogle Maps APIキーを貼り付けてください。
+// Google Cloud Console → APIとサービス → 認証情報 で発行したキーです。
+// このファイルはリポジトリにそのままアップロードして大丈夫です
+// （APIキー側で「fukabot.github.io/*」からのアクセスのみ許可する制限をかけてあるため）。
+
+const GOOGLE_MAPS_API_KEY = AIzaSyDYG7Hu4uwvtr_qlZGt7MhgvKhfnd9C6Es;
